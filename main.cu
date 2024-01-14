68ef6af27cefd5164bff832a4a901d7379de46ad
mjorrico <jordanenrico214@gmail.com>
2024-01-14 19:44:35 +0100

    init

---
+++ main.cu
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdlib>
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cout << "Matrix size is required." << std::endl;
+        exit(1);
+    }
+
+    int N = 1 << atoi(argv[1]);
+    int bytes = N * N * sizeof(float);
+    double gflop = (2 * (double)N - 1) * N * N * 1e-9;
+    double memoryio = 3 * N * N * sizeof(float) * 1e-6;
+    std::cout << "Matrix size: " << N << " x " << N << std::endl;
+    std::cout << "Total FLOP: " << gflop << " GFLOP" << std::endl;
+    std::cout << "Memory I/O: " << memoryio << " MB" << std::endl << std::endl;
+
+    float* a = (float*)malloc(bytes);
+    float* b = (float*)malloc(bytes);
+    float* c = (float*)malloc(bytes);
+
+    free(a);
+    free(b);
+    free(c);
+}
